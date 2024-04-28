d97156b15ff2596a60d29ab2b6c091b18fbea4e6
ShariarM <shariar01976@gmail.com>
2024-04-29 00:48:35 +0600

    added many thing

--- src/Shared/Leftsidenav/Leftsidenav.jsx
+++ src/Shared/Leftsidenav/Leftsidenav.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const Leftsidenav = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
     return (
         <div>
-           <h2 className="text-2xl">Left Side</h2>            
+            <h2 className="text-2xl">All Categories</h2>
+            {
+                categories.map(category =><Link className="block ml-4 mb-4 text-xl font-bold" key={category.id}>{category.name}</Link>)
+            }
         </div>
     );
 };
--- src/Shared/Rightsidenav/Rightsidenav.jsx
+++ src/Shared/Rightsidenav/Rightsidenav.jsx
@@ -1,5 +1,7 @@
 import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from 'react-icons/fa';
-
+import qZone1 from '../../assets/qZone1.png'
+import qZone2 from '../../assets/qZone2.png'
+import qZone3 from '../../assets/qZone3.png'
 const Rightsidenav = () => {
     return (
         <div>
@@ -30,6 +32,14 @@ const Rightsidenav = () => {
                     <FaInstagram className='mr-2'></FaInstagram>
                     <span>Instagram</span>
                 </a>
+            </div>
+            {/* q zon */}
+            <div className='p-4 space-y-3 mb-4'>
+                <h2 className="text-2xl">Q Zone</h2>
+                <img src={qZone1} alt="" />
+                <img src={qZone2} alt="" />
+                <img src={qZone3} alt="" />
+                
             </div>
         </div>
     );
